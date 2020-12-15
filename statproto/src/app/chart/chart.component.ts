51d88f5caf0ac5fe42ccbef7bc5f69cdea4c04a9
GregorAT <gregor.dav2002@gmail.com>
2020-12-15 15:42:06 +0100

    Title Input

--- statproto/src/app/chart/chart.component.ts
+++ statproto/src/app/chart/chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Chart } from 'angular-highcharts';
 @Component({
   selector: 'app-chart',
@@ -7,40 +7,51 @@ import { Chart } from 'angular-highcharts';
 })
 export class ChartComponent implements OnInit {
 
-  chart = new Chart({
+  @Input()
+  charttitle = "";
+   linechart : Chart
 
-    title: {
-      text: 'Corona-Neuinfektionen'
-    },
-    credits: {
-      enabled: false
-    },
-    xAxis:{
-      type: 'datetime',
-      labels: {
-        format: '{value:%Y-%m-%d}',
-    
-      }
-    },
-    series: [
-      
-      {
-        type: 'line',
-        name: 'Österreich',
-        data: [9262,9586, 7063, 5665, 4657, 5984]
-      },
-      {
-        type: 'line',
-        name: 'OÖ',
-        data: [2554, 1891, 1665, 1041, 917, 1230]
-      },
 
-    ]
-  });
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.charttitle);
+    this.newChart();
 
+  }
+
+  newChart() {
+    this.linechart = new Chart({
+
+      title: {
+        text: this.charttitle
+      },
+      credits: {
+        enabled: false
+      },
+      xAxis: {
+        type: 'datetime',
+        labels: {
+          format: '{value:%Y-%m-%d}',
+
+        }
+      },
+      series: [
+
+        {
+          type: 'line',
+          name: 'Österreich',
+          data: [9262, 9586, 7063, 5665, 4657, 5984]
+        },
+        {
+          type: 'line',
+          name: 'OÖ',
+          data: [2554, 1891, 1665, 1041, 917, 1230]
+        },
+
+      ]
+    });
   }
 
 }
--- statproto/src/app/components/daten/daten.component.ts
+++ statproto/src/app/components/daten/daten.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatenComponent implements OnInit {
 
+  
+  
   constructor() { }
 
   ngOnInit(): void {
