0166868e84c2412318d6e7d1f5e12413291030a0
Jakob Hocheneder <jakob.hocheneder@gmail.com>
2021-01-11 17:42:32 +0100

    started with validations

--- statproto/src/app/components/register/register.component.ts
+++ statproto/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  username: string
+  email: string
+  emailrepeat: string
+  password: string
+  passwordrepeat: string
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  exampleForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    emailrepeat: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+    passwordrepeat: new FormControl('', Validators.required),
+});
 
 }
