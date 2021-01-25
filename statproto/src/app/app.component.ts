c55e2bcbd9d97f0fb41914398faf11182eafc22c
Jakob Hocheneder <jakob.hocheneder@gmail.com>
2021-01-25 13:49:00 +0100

    Added logout button

--- statproto/src/app/app.component.ts
+++ statproto/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { AuthGuard } from './guards/auth.guard';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +10,23 @@ import { Title } from '@angular/platform-browser';
 })
 export class AppComponent {
   title = 'Statsmart';
-  public constructor(private titleService: Title) {
+
+  public constructor(
+    private titleService: Title,
+    private authGuard: AuthGuard,
+    private authService: AuthService) {
     this.titleService.setTitle("Statsmart");
   }
 
   public setTitle(newTitle: string) {
     this.titleService.setTitle(newTitle);
+  }
+
+  isLogin(){
+    return this.authGuard.isAuthenticated()
+  }
+
+  logout(){
+    this.authService.logout()
   }
 }
